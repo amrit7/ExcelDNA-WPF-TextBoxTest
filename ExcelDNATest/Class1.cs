f95a825653e62bf9f29273a2668c8c34fcd42871
amrit7 <asparmar7@yahoo.com>
2024-04-23 10:12:08 +0100

    Test - Text Box not working

---
+++ ExcelDNATest/Class1.cs
@@ -0,0 +1,17 @@
+﻿using ExcelDna.Integration;
+using WpfApp1;
+
+namespace ExcelDNATest
+{
+    public static class MyFunctions
+    {
+        [ExcelFunction(Description = ".NET Function")]
+        public static string SayHello(string name)
+        {
+            var frm = new MainWindow();
+            frm.Show();
+
+            return "Hello " + name;
+        }
+    }
+}
